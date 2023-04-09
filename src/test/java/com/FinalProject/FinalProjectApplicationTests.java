604b221e205ac62234ea4ad91272c82109910fe3
KamilAliyev1 <kamilaliyev240@gmail.com>
2023-04-09 18:37:51 +0400

    fixing

--- src/test/java/com/FinalProject/FinalProjectApplicationTests.java
+++ src/test/java/com/FinalProject/FinalProjectApplicationTests.java
@@ -1,12 +1,11 @@
 package com.FinalProject;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FinalProjectApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
