c2890d4e9f700dfe77e86cba701fec1966d5f1d4
KamilAliyev1 <kamilaliyev240@gmail.com>
2023-04-09 13:07:09 +0400

    first commit

---
+++ src/test/java/com/FinalProject/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
