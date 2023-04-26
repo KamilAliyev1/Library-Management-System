c4090b7e90f0f6cb48b7d5cd13d93b405566a812
Rahim Babali <rahimbabali@gmail.com>
2023-04-26 15:28:11 +0400

    new update

--- src/main/java/com/FinalProject/controller/StudentController.java
+++ src/main/java/com/FinalProject/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.FinalProject.controller;
 
 import com.FinalProject.dto.studentdto.StudentDto;
+import com.FinalProject.model.Student;
 import com.FinalProject.service.StudentService;
 import lombok.RequiredArgsConstructor;
 
@@ -12,4 +13,9 @@ public class StudentController {
     public List<StudentDto> getStudentList(){
         return studentService.getStudentList();
     }
+
+    public Student findById(Long id) {
+        return studentService.findById(id)
+    }
 }
+
