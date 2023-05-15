24d951e85cceb9e52de76cd6c3255696375935b6
Rahim Babali <rahimbabali@gmail.com>
2023-05-15 16:17:49 +0400

    thymeleaf student-list and create update

--- src/main/java/com/FinalProject/controller/StudentController.java
+++ src/main/java/com/FinalProject/controller/StudentController.java
@@ -1,29 +1,49 @@
 package com.FinalProject.controller;
 
+import com.FinalProject.dto.BookDto;
 import com.FinalProject.dto.studentdto.CreateStudentDto;
 import com.FinalProject.dto.studentdto.StudentDto;
 import com.FinalProject.dto.studentdto.UpdateStudentDto;
 import com.FinalProject.model.Student;
-import com.FinalProject.service.StudentService;
+import com.FinalProject.service.impl.StudentServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/student")
 @RequiredArgsConstructor
 public class StudentController {
-    private final StudentService studentService;
+    private final StudentServiceImpl studentService;
+    @GetMapping("/add")
+    public String studentForm(CreateStudentDto studentRequest) {
+        return "student-create";
+    }
 
     @GetMapping("/getAll")
     public List<StudentDto> getStudentList() {
         return studentService.getStudentList();
     }
+    @GetMapping
+    public String findAll(Model model) {
+        final var students = studentService.getStudentList();
+        model.addAttribute("students", students);
+        return "student-list";
+    }
 
-    @PostMapping("/create")
-    public Long createStudent(CreateStudentDto studentDto) {
-        return studentService.createStudent(studentDto);
+    @PostMapping
+    public String createStudent(@ModelAttribute("studentRequest") CreateStudentDto studentRequest, Model model) {
+        if (studentRequest==null){
+            return "student-create";
+        }
+        else {
+            studentService.createStudent(studentRequest);
+            model.addAttribute("studentRequest", studentService.getStudentList());
+            return "redirect:/student";
+        }
 
     }
 
