02e406c00981d3e8085ed2dd72f60c783c6aeaad
huseynhasanov07 <hasanov.huseyn07@gmail.com>
2023-06-26 10:13:39 +0400

    Book commit

--- src/main/java/com/FinalProject/exception/FileAlreadyExistsException.java
+++ src/main/java/com/FinalProject/exception/FileAlreadyExistsException.java
@@ -1,19 +1,8 @@
 package com.FinalProject.exception;
 
-<<<<<<< HEAD
+
 public class FileAlreadyExistsException extends RuntimeException {
-    public FileAlreadyExistsException() {
-    }
-
-    public FileAlreadyExistsException(String file) {
-        super(file);
-=======
-public class FileAlreadyExistsException extends RuntimeException{
-    public FileAlreadyExistsException() {
-    }
-
     public FileAlreadyExistsException(String message) {
         super(message);
->>>>>>> origin/main
     }
 }
