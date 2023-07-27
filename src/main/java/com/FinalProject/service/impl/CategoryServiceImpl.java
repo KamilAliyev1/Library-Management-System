102cb9b4443a8387bc011bc5615cd4617d768ff8
Puruza Macidova <mecidova005@gmail.com>
2023-07-27 15:22:55 +0400

    fixed categories

--- src/main/java/com/FinalProject/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/FinalProject/service/impl/CategoryServiceImpl.java
@@ -2,16 +2,21 @@ package com.FinalProject.service.impl;
 
 import com.FinalProject.dto.BookDto;
 import com.FinalProject.dto.CategoryDto;
+import com.FinalProject.exception.BookNotFoundException;
 import com.FinalProject.exception.CategoryAlreadyExistsException;
 import com.FinalProject.exception.CategoryNotFoundException;
 import com.FinalProject.mapper.CategoryMapper;
+import com.FinalProject.model.Book;
 import com.FinalProject.model.Category;
+import com.FinalProject.repository.BookRepository;
 import com.FinalProject.repository.CategoryRepository;
 import com.FinalProject.service.CategoryService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static com.FinalProject.service.impl.BookServiceImpl.entityListToResponseList;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -21,11 +26,13 @@ public class CategoryServiceImpl implements CategoryService {
     private final CategoryMapper categoryMapper;
 
     private final BookServiceImpl bookService;
+    private final BookRepository bookRepository;
 
-    public CategoryServiceImpl(CategoryRepository categoryRepository, CategoryMapper categoryMapper, BookServiceImpl bookService) {
+    public CategoryServiceImpl(CategoryRepository categoryRepository, CategoryMapper categoryMapper, BookServiceImpl bookService, BookRepository bookRepository) {
         this.categoryRepository = categoryRepository;
         this.categoryMapper = categoryMapper;
         this.bookService = bookService;
+        this.bookRepository = bookRepository;
     }
 
     @Override
@@ -64,9 +71,13 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void updateCategory(Long id, CategoryDto categoryDto) {
-        Category category = categoryRepository.findById(id).get();
-        category.setName(categoryDto.getName());
-        categoryRepository.save(category);
+        if (categoryRepository.findById(id).isPresent()) {
+            Category existingCategory = categoryRepository.findById(id).get();
+            existingCategory.setName(categoryDto.getName());
+            Category updatedCategory = categoryRepository.save(existingCategory);
+        } else {
+            System.out.println("Category is not present");
+        }
 
 
     }
@@ -78,4 +89,24 @@ public class CategoryServiceImpl implements CategoryService {
                 () -> new CategoryNotFoundException("Not found Category such id= " + id));
         categoryRepository.deleteById(category.getId());
     }
+
+//    @Override
+//    public List<BookDto> showBooksByCategoryName(String name) {
+//        List<BookDto> bookDtoList=bookService.findAll();
+//        List<BookDto> categoryBook=new ArrayList<>();
+//        for(BookDto bookDto : bookDtoList){
+//            if(bookDto.getCategory()==name)
+//                categoryBook.add(bookDto);
+//        }
+//        return categoryBook;
+//    }
+
+    @Override
+    public List<BookDto> showBooksByCategoryName(String category) {
+        if (bookRepository.findByCategory(category).isEmpty())
+            throw new BookNotFoundException("Book not found with category :  " + category);
+       List<Book>  book=bookRepository.findByCategory(category);
+        return entityListToResponseList(bookRepository.findByCategory(category));
+    }
 }
+
